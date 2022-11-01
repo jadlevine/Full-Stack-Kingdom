0339c4f8a68e8436a95861d81b419ecda8d43438
Josh Levine <levine.joshuaadam@gmail.com>
2022-11-01 13:40:14 -0400

    after lunch

--- client/src/components/AttractionsList.js
+++ client/src/components/AttractionsList.js
@@ -1,11 +1,15 @@
 import AttractionCard from './AttractionCard'
 import axios from 'axios'
 const BASE_URL = '/api'
+import { useState } from 'react'
 
 const AttractionsList = () => {
+  const [attractions, setAttractions] = useState([])
+
   //axios call to grab attractions
   const getAllAttractions = async () => {
     const response = await axios.get(`${BASE_URL}/attractions`)
+    setAttractions(response.data)
     console.log(response)
   }
 
@@ -13,8 +17,11 @@ const AttractionsList = () => {
 
   return (
     <div>
-      {/* map through attractions array and render for each AttractionsList */}
-      <AttractionCard />
+      <div className="container">
+        {attractions.map((attraction) => (
+          <AttractionCard key={attraction._id} attraction={attraction} />
+        ))}
+      </div>
     </div>
   )
 }
