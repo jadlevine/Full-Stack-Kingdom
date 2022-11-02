cb4e06a639a43d67896331ff8eaaed9fa7745418
Olivia Schwartz <ocmooney@gmail.com>
2022-11-02 10:31:05 -0400

    Initial Deployment

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes) //this line says, all my routes are in routes, which I required above
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 //should stay at bottom
 app.listen(PORT, () => {
